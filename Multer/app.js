17674744969f306680a2ad6143121b37b8700a12
CardinPatson <cardin.tiako@gmail.com>
2022-04-20 23:34:04 +0200

    Make url with image in folder

--- Multer/app.js
+++ Multer/app.js
@@ -1,25 +1,58 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const fs = require("fs");
 const middleware = require("./middleware/image");
 
 const cors = require("cors");
+const imagePath = path.join(__dirname, "images");
 
 app.use(cors());
 app.use(express.json());
-app.use("/images", express.static(path.join(__dirname, "images")));
+app.use("/images", express.static(imagePath));
 
-app.post("/post", middleware.single("image"), (req, res) => {
+app.get("/", (req, res) => {
+	res.sendFile(__dirname + "/index.html");
+});
+
+//POST IMAGES
+app.post("/images", middleware.single("image"), (req, res) => {
 	try {
-		const url_image = `${req.protocol}://${req.get("host")}/images/${
-			req.file.filename
-		}`;
+		//READ IMAGE FOLDER
+		fs.readdir(imagePath, function (err, images) {
+			if (err) {
+				throw err;
+			}
+			//MAKE URL WITH IMAGE IN THE FOLDER
+			const urlImages = images.map((image) => {
+				return `${req.protocol}://${req.get("host")}/images/${image}`;
+			});
 
-		res.status(200).json({ url_image });
+			res.status(200).json({ url: urlImages });
+		});
 	} catch (error) {
 		res.status(500).json({ error });
 	}
 });
-app.listen(3001, () => {
-	console.log("running on port 3001");
+
+app.get("/images", (req, res) => {
+	try {
+		//READ IMAGE FOLDER
+		fs.readdir(imagePath, function (err, images) {
+			if (err) {
+				throw err;
+			}
+			//MAKE URL WITH IMAGE IN THE FOLDER
+			const urlImages = images.map((image) => {
+				return `${req.protocol}://${req.get("host")}/images/${image}`;
+			});
+
+			res.status(200).json({ url: urlImages });
+		});
+	} catch (error) {
+		res.status(500).json({ error });
+	}
+});
+app.listen(8080, "0.0.0.0", () => {
+	console.log("running on port 8080");
 });
